3279e3dc529d003261eca96d955857a6da950811
HONOR <89228157157@mail.ru>
2024-09-07 03:27:25 +0500

    Добавлена функция открытие изображения в новом

--- cats.py
+++ cats.py
@@ -3,9 +3,6 @@ from PIL import  Image, ImageTk
 import requests
 from io import BytesIO
 
-from keras.src.legacy.backend import update
-
-
 def load_image(url):
     try:
         response = requests.get(url)
@@ -18,18 +15,19 @@ def load_image(url):
         print(f"Произошла ошибка: {e}")
         return None
 
-
-def set_image():
+def open_new_window():
     img = load_image(url)
 
     if img:
-        label.config(image=img)
+        new_window = Toplevel()
+        new_window.title("")
+        new_window.geometry("600x480")
+        label = Label(new_window, image=img)
+        label.pack()
         label.image = img
-
 
 def exit():
     window.destroy()
-
 
 window = Tk()
 window.title("Cats!")
@@ -46,12 +44,12 @@ window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 
-url = "https://cataas.com/"
+url = "https://cataas.com/cat"
 img = load_image(url)
 
 if img:
