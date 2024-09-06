74b7e125983a06d6f03f7d0fca8f28a0329336b9
HONOR <89228157157@mail.ru>
2024-09-07 02:47:10 +0500

    Создана функция получения изображения.

--- cats.py
+++ cats.py
@@ -4,14 +4,27 @@ import requests
 from io import BytesIO
 
 
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+
+
+
 window = Tk()
-window.title("Cataas!")
+window.title("Cats!")
 window.geometry("600x480")
 
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
+url = "https://cataas.com/"
 img = load_image(url)
 
 if img:
